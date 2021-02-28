07557abb3a2de704fa135d6f44e13df1f4c0f94f
Roopa <roopa.grsv@gmail.com>
2021-02-28 13:24:52 +0530

    d

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 version = "=2.46.0"
   subscription_id = "eb3968fe-3761-4c75-94e5-0e8c95e5e4c9"
   client_id       = "15d2a6fa-ffdc-4ad8-b941-8bd5d3b87f34"
-  client_secret   = "p7MZMtRmGj2f25Q~40Mem0~.qGcux22-__"
+  client_secret   = "a68e8c72-eccf-4869-826e-145e77cf8fe8"
   tenant_id       = "7829b58f-8707-4a6d-bf5f-d8f7ac3c18ea"
 }
 resource "azurerm_resource_group" "example" {
