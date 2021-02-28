d5960ad03ed697db5678a57b07c36da8e992238d
Roopa <roopa.grsv@gmail.com>
2021-02-28 13:19:09 +0530

    d

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 version = "=2.46.0"
   subscription_id = "eb3968fe-3761-4c75-94e5-0e8c95e5e4c9"
-  client_id       = "749f3099-215c-40f5-9a6c-2558e4b0ae0e"
+  client_id       = "15d2a6fa-ffdc-4ad8-b941-8bd5d3b87f34"
   client_secret   = "p7MZMtRmGj2f25Q~40Mem0~.qGcux22-__"
   tenant_id       = "7829b58f-8707-4a6d-bf5f-d8f7ac3c18ea"
 }
